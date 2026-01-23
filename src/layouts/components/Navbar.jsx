74993b5511f6cadedfe058d7da899bdabd30f4ad
anggarchmdi <anggarchmdi@gmail.com>
2026-01-23 09:20:10 +0700

    feat: add components implementatio

---
+++ src/layouts/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-full bg-yellow-500 p-4'>
+        <div className="grid grid-cols-2">
+            <div className="">
+                <div className="">
+                    <h1 className='text-xl font-rotobo text-[#] font-bold'>Pokemon</h1>
+                </div>
+                <div className=""></div>
+            </div>
+            <div className="">
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
