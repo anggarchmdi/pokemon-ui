6e8373a2db3148cd8733caebc39db7f26a047846
anggarchmdi <anggarchmdi@gmail.com>
2026-01-23 09:06:02 +0700

    feat: add main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,9 @@
+import Navbar from "./components/Navbar"
+
 export default function MainLayout({children}) {
     return (
     <div className="min-h-screen bg-gray-50">
-      <header className="bg-blue-600 text-white p-4 shadow">
-        <h1 className="text-lg font-semibold">Dashboard</h1>
-      </header>
+     <Navbar />
       <main className="p-6">{children}</main>
     </div>
     )
