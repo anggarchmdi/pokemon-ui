5efd683252d85d2c726f22620f625cc3cda4c09a
anggarchmdi <anggarchmdi@gmail.com>
2026-01-23 09:23:01 +0700

    fix: update api utilities

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,14 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "",
-    withCredentials: true,
+    baseURL: import.meta.env.Vite_API_URL,
+    timeout: 1000,
 });
-
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if(token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-});
-
 export default api;
