099476d9566f021e30aa4fc11f79c25fed70951a
anggarchmdi <anggarchmdi@gmail.com>
2026-01-23 09:07:18 +0700

    feat: implement notification toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import AppRouter from "./routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
-  return <AppRouter />;
+  return (
+    <>
+      <AppRouter />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
+    </>
+  );
 }
