b862201b75324f3c05c623e91311842e9bef28b3
anggarchmdi <anggarchmdi@gmail.com>
2026-01-30 19:18:45 +0700

    feat: update logic & style at Footer and Floating components

--- src/layouts/components/Floating.jsx
+++ src/layouts/components/Floating.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Ball from '../../../public/ball.gif'
+import { useAuthStore } from '../../store/authStore'
 
 function Floating() {
+  const user = useAuthStore(state => state.user)
+  const username = user?.username || 'Bro'
+
+  const messages = [
+  `hey ${username}! Jangan lupa minum air ya 💧`,
+  `hey ${username}, Madrid kapan treble🤣🤣`,
+  `Cari Pokémon atau cari jodoh nih, ${username}? 🤔`,
+  `nunggu 19jt lap. pekerjaan kek nunggu Barca juara UCL🥲`,
+  `hey ${username}, 4 sehat 5 nggak nambah nambah ya!😆`,
+  `${username}, saksikanlah kebangkitan Setan Merah!😉`,
+  `Yang penting 15 UCL ya ${username}🫠`,
+  `bercandaa yaa ${username}....Slebewwwww🗿`,
+  `hey ${username}, udah mengapresiasi pemerintah belum hari ini?🤣`,
+  `${username}, hidup itu keras… tapi cicilan lebih keras 🤣`,
+  `hey ${username}, udah panen sawit belum hari ini?🤣`,
+  `hey ${username} apapun yang terjadi, teruslah bernafas!`,
+  ]
+
+  const [text, setText] = useState(messages[0])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * messages.length)
+      setText(messages[randomIndex])
+    }, 3000)
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
-    <div>Floating</div>
+    <div className="fixed bottom-6 right-6 z-50 lg:flex items-end gap-3 group hidden">
+      <div className="
+        px-4 py-2
+        bg-white text-gray-800 text-sm font-semibold
+        rounded-xl shadow-lg
+        opacity-0 translate-y-2
+        group-hover:opacity-100 group-hover:translate-y-0
+        transition-all duration-300
+        whitespace-nowrap
+      ">
+        {text}
+      </div>
+
+      <div className="
+        w-12 h-12
+        group-hover:animate-spin
+        cursor-pointer
+      ">
+        <img src={Ball} alt="Pokeball" />
+      </div>
+    </div>
   )
 }
 
--- src/layouts/components/Footer.jsx
+++ src/layouts/components/Footer.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import React from "react"
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa"
+import { useNavigate } from "react-router-dom"
 
 function Footer() {
+  const navigate = useNavigate()
   return (
-    <div>Footer</div>
+    <footer className="bg-gray-900 text-white">
+      <div className="w-full h-[1px] bg-gradient-to-r from-transparent via-yellow-400 to-transparent" />
+      <div className="px-10 py-4 grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div>
+          <h1 className="text-2xl font-bold">
+            <span className="text-yellow-400">Poké</span>Dex
+          </h1>
+          <p className="text-sm text-gray-400 mt-3 leading-relaxed">
+            Explore Pokémon data using public API.  
+            Built with React, Vite, Axios, and modern UI practices.
+          </p>
+        </div>
+
+        <div>
+          <h2 className="font-semibold mb-4">Menu</h2>
+          <ul className="space-y-2 text-sm text-gray-400">
+            <li onClick={() => navigate('/')} className="hover:text-yellow-400 cursor-pointer">Home</li>
+            <li onClick={() => navigate('/pokemon')} className="hover:text-yellow-400 cursor-pointer">Pokémon</li>
+            <li className="hover:text-yellow-400 cursor-pointer">About</li>
+          </ul>
+        </div>
+
+        <div>
+          <h2 className="font-semibold mb-4">Connect</h2>
+          <div className="flex gap-4 text-xl text-gray-400">
+            <a
+              href="#"
+              className="hover:text-yellow-400 transition"
+              aria-label="GitHub"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="#"
+              className="hover:text-yellow-400 transition"
+              aria-label="Instagram"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="#"
+              className="hover:text-yellow-400 transition"
+              aria-label="LinkedIn"
+            >
+              <FaLinkedin />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="text-center text-sm text-white py-4">
+        © {new Date().getFullYear()} PokéDex by Angga Rachmadi | All rights reserved.
+      </div>
+    </footer>
   )
 }
 
