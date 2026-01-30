1f6f7f54713f5f6505c6691439ca2a9f873f279e
anggarchmdi <anggarchmdi@gmail.com>
2026-01-30 19:24:57 +0700

    refactor: change type from Pokemon

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -37,7 +37,7 @@ function Register() {
   return (
     <div className="w-full">
       <div className="w-full h-12 bg-gradient-to-br mb-4 from-[#FF6500] to-yellow-400 flex justify-center text-center bg-clip-text">
-      <h1 className='font-bold text-[2rem] font-poppins text-transparent text-center'>Pokemon</h1>
+      <h1 className='font-bold text-[2rem] font-poppins text-transparent text-center'>Pokémon</h1>
       </div>
     <form onSubmit={handleSubmit} className="space-y-3">
       <input
