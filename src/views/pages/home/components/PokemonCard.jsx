d969487ffa9ddd3be2c820b99f151f286cdfeafa
anggarchmdi <anggarchmdi@gmail.com>
2026-02-05 23:15:47 +0700

    style(ui): improve ui for card pokemon

--- src/views/pages/home/components/PokemonCard.jsx
+++ src/views/pages/home/components/PokemonCard.jsx
@@ -1,9 +1,10 @@
 function PokemonCard({ pokemon }) {
   return (
-    <div className="px-3 h-56"  data-aos="fade">
-      <div className="rounded-lg w-40 h-56 bg-white backdrop-blur transition-all p-5 text-center flex justify-center items-center">
-        <div className="absolute w-20 h-20 bg-yellow-400 rounded-full -translate-y-9"></div>
-        <div className="absolute w-32 h-32 backdrop-blur-xl rounded-2xl -translate-y-9"></div>
+    <div className="h-56">
+      <div className="w-44 h-60 bg-red-500 p-2 rounded-lg">
+      <div className="rounded-lg w-40 h-56 bg-rose-700 backdrop-blur transition-all p-5 text-center flex justify-center items-center">
+        <div className="absolute w-20 h-20 bg-rose-400 rounded-full -translate-y-9"></div>
+        <div className="absolute w-36 h-36 backdrop-blur-lg rounded-2xl -translate-y-9"></div>
         <div className="">
         <img
           src={pokemon.sprites.other['official-artwork'].front_default}
@@ -11,20 +12,21 @@ function PokemonCard({ pokemon }) {
           className="w-32 h-32 drop-shadow-2xl"
           />
 
-        <h3 className="font-bold capitalize mt-4">
+        <h3 className="font-bold font-montserrat capitalize mt-4 text-white">
           {pokemon.name}
         </h3>
         <div className="flex justify-center gap-x-2 mt-3">
           {pokemon.types.map(t => (
               <span
               key={t.type.name}
-              className="text-xs px-3  py-1 rounded-full bg-yellow-600 text-yellow-200 font-bold"
+              className="text-xs px-3  py-1 rounded-full bg-yellow-300 text-amber-800 font-bold"
               >
               {t.type.name}
             </span>
           ))}
           </div>
         </div>
+      </div>
       </div>
     </div>
   )
