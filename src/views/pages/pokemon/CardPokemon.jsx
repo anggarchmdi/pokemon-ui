3e09b1dd10ec6271fe849edc2db398db955f26a0
anggarchmdi <anggarchmdi@gmail.com>
2026-02-06 12:57:06 +0700

    fix(ui): change the entire view on the pokemon card

--- src/views/pages/pokemon/CardPokemon.jsx
+++ src/views/pages/pokemon/CardPokemon.jsx
@@ -13,29 +13,37 @@ function CardPokemon({ pokemons }) {
               })
           })
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-x-8">
+    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 gap-x-6 py-8">
       {pokemons.map(pokemon => (
         <div
           key={pokemon.id}
           onClick={() => navigate(`/pokemon/${pokemon.name}`)}
-          className="relative 2xl:w-72 h-56 bg-[#FF6500] rounded-2xl shadow-lg  p-5
+          className="relative 2xl:w-56 w-40 md:w-44 lg:w-52 xl:w-48 h-56 bg-red-500 rounded-2xl shadow-lg px-4 py-2
                     hover:scale-95 duration-300
-                     transition-all cursor-pointer flex justify-center flex-col items-center "
+                     transition-all cursor-pointer flex justify-center flex-col items-center"
         >
-          <div className="w-14 h-14 bg-yellow-200 absolute rounded-full z-30"></div>
-            <div className="w-20 h-20 bg-yellow-300 absolute rounded-full z-20"></div>
-            <div className="w-24 h-24 bg-[#FF6500] absolute rounded-full z-10"></div>
-            <div className="2xl:w-56 lg:w-48 md:w-40 w-32 h-56 rounded-2xl backdrop-filter backdrop-blur-xl absolute z-40"></div>
-          <div className="absolute z-40">
-          <img
-            src={pokemon.sprites.other['official-artwork'].front_default}
-            alt={pokemon.name}
-            className="w-32 h-32 mx-auto" 
-            data-aos="fade"
-            />
-          <h3 className="text-center font-bold font-rotobo text-white capitalize mt-4">
-            {pokemon.name}
-          </h3>
+          <div className="w-36 md:w-40 lg:w-48 xl:w-44 2xl:w-52 h-52 bg-rose-700 rounded-lg backdrop-blur transition-all p-5 text-center flex justify-center items-center">
+            <div className="absolute w-20 h-20 bg-rose-400 rounded-full"></div>
+            <div className="absolute w-36 h-36 backdrop-blur-lg rounded-2xl"></div>
+            <div className="relative flex flex-col justify-center items-center">
+              <img src={pokemon.sprites.other['official-artwork'].front_default}
+                alt={pokemon.name}
+                className="w-32 h-32" 
+                data-aos="fade" />
+                <h3 className="text-center font-bold font-rotobo text-white capitalize mt-4">
+                {pokemon.name}
+                </h3>
+                <div className="flex justify-center gap-x-2">
+          {pokemon.types.map(t => (
+              <span
+              key={t.type.name}
+              className="text-xs px-3  py-1 rounded-full bg-yellow-300 text-amber-800 font-bold"
+              >
+              {t.type.name}
+            </span>
+          ))}
+          </div>
+            </div>
           </div>
         </div>
       ))}
